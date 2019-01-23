e9bf479ada6f5c10c7449f64259f369866a6ffed
Rodrigo Dantas <rodrigodantas.91@gmail.com>
2019-01-23 17:31:59 -0400

    Modeling Books Entity

---
+++ src/main/java/com/gb/challenge/enums/Language.java
@@ -0,0 +1,11 @@
+package com.gb.challenge.enums;
+
+public enum Language {
+
+    AA, AB, AE, AF, AL, AM, AR, AS, AY, AZ, BA, BE, BG, BH, BI, BN, BO, BP, BR, CA, CF, CH, CO, CR, CS, CY, DE, DR, DT,
+    DZ, EF, EG, EJ, EL, EN, EO, EP, ER, ES, ET, EU, FA, FE, FI, FJ, FO, FR, FY, GA, GD, GL, GN, GU, HA, HE, HI, HM, HR,
+    HU, HY, IA, IE, IK, IL, IN, IS, IT, IW, JA, JI, JV, KA, KK, KL, KM, KN, KO, KS, KU, KY, LA, LN, LO, LS, LT, LV, MG,
+    MI, MK, ML, MN, MO, MR, MS, MT, MY, NA, ND, NE, NL, NO, NT, OC, OF, OG, OM, OR, PA, PL, PN, PS, PT, QU, RE, RM, RN,
+    RO, RU, RW, SA, SC, SD, SF, SG, SH, SI, SK, SL, SM, SN, SO, SQ, SR, SS, ST, SU, SV, SW, TA, TE, TG, TH, TI, TJ, TK,
+    TL, TN, TO, TP, TR, TS, TT, TW, UK, UL, UR, UZ, VI, VO, WO, XH, YO, ZG, ZH, ZU
+}
---
+++ src/main/java/com/gb/challenge/model/Book.java
@@ -0,0 +1,46 @@
+package com.gb.challenge.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.gb.challenge.enums.Language;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "book")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @Column(name = "title", unique = false, nullable = false, length = 256)
+    private String title;
+
+    @Column(name = "description", unique = false, nullable = false, length = 512)
+    private String description;
+
+    @Column(name = "isbn", unique = true, nullable = false)
+    private Long isbn;
+
+    @Column(name = "Language", unique = false, nullable = false, length = 2)
+    private Language language;
+
+}
