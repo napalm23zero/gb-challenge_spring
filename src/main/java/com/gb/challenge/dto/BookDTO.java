12039447f06a212dd6439c9f377b25e065741add
Rodrigo Dantas <rodrigodantas.91@gmail.com>
2019-01-24 09:49:40 -0400

    Building DTO

--- src/main/java/com/gb/challenge/dto/BookDTO.java
+++ src/main/java/com/gb/challenge/dto/BookDTO.java
@@ -1,5 +1,34 @@
 package com.gb.challenge.dto;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+import com.gb.challenge.enums.Language;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookDTO {
+
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private Long isbn;
+
+    @Enumerated(EnumType.STRING)
+    private Language language;
 
 }
