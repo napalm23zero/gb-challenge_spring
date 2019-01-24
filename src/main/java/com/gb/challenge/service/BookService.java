943ad361e74cae28afd82006be9dbd5793f24079
Rodrigo Dantas <rodrigodantas.91@gmail.com>
2019-01-24 09:28:04 -0400

    Implementing Service Base

--- src/main/java/com/gb/challenge/service/BookService.java
+++ src/main/java/com/gb/challenge/service/BookService.java
@@ -7,34 +7,21 @@ import com.gb.challenge.dto.BookDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort.Direction;
 
-public class BookService {
+public interface BookService {
 
-    public BookDTO create(BookDTO book) {
-        return null;
-    }
+    public BookDTO create(BookDTO book);
 
-    public BookDTO read( Long id) {
-        return null;
-    }
+    public BookDTO read(Long id);
 
-    public BookDTO update(@Valid BookDTO book) {
-        return null;
-    }
+    public BookDTO update(@Valid BookDTO book);
 
-    public void delete(Long id) {
-    }
+    public void delete(Long id);
 
-    public Page<BookDTO> list(Integer pageNumber, Integer pageSize, Direction direction, String orderBy) {
-        return null;
-    }
+    public Page<BookDTO> list(Integer pageNumber, Integer pageSize, Direction direction, String orderBy);
 
     public Page<BookDTO> search(BookDTO book, Integer pageNumber, Integer pageSize, Direction direction,
-            String orderBy) {
-        return null;
-    }
+            String orderBy);
 
-    public Page<BookDTO> find(BookDTO book, Integer pageNumber, Integer pageSize, Direction direction, String orderBy) {
-        return null;
-    }
+    public Page<BookDTO> find(BookDTO book, Integer pageNumber, Integer pageSize, Direction direction, String orderBy);
 
 }
--- src/main/java/com/gb/challenge/service/impl/BookServiceImpl.java
+++ src/main/java/com/gb/challenge/service/impl/BookServiceImpl.java
@@ -1,5 +1,59 @@
 package com.gb.challenge.service.impl;
 
-public class BookServiceImpl {
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+
+import com.gb.challenge.dto.BookDTO;
+import com.gb.challenge.model.Book;
+import com.gb.challenge.repository.BookRepository;
+import com.gb.challenge.service.BookService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+@Service("bookService")
+@Transactional
+public class BookServiceImpl extends GenericServiceImpl<Book, Long> implements BookService {
+
+    @Autowired
+    private BookRepository repository;
+
+    @Override
+    public BookDTO create(BookDTO book) {
+        return null;
+    }
+
+    @Override
+    public BookDTO read(Long id) {
+        return null;
+    }
+
+    @Override
+    public BookDTO update(@Valid BookDTO book) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+
+    @Override
+    public Page<BookDTO> list(Integer pageNumber, Integer pageSize, Direction direction, String orderBy) {
+        return null;
+    }
+
+    @Override
+    public Page<BookDTO> search(BookDTO book, Integer pageNumber, Integer pageSize, Direction direction,
+            String orderBy) {
+        return null;
+    }
+
+    @Override
+    public Page<BookDTO> find(BookDTO book, Integer pageNumber, Integer pageSize, Direction direction, String orderBy) {
+        return null;
+    }
 
 }
